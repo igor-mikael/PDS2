14ff36f89245716e9b879a2d921e4003a9e62f54
igor-mikael <56322613+igor-mikael@users.noreply.github.com>
2019-11-18 20:35:00 -0400

    login fb

---
+++ login/server/services.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.upsert(
+    {	service:	"facebook"	},
+    {	
+            $set:	{
+                    appId:	"561633557978786",
+                    secret:	"e31218eb9c32784921dc7abdc354696e"
+            }
+    }
+);
